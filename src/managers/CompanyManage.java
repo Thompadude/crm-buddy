2101714e7a7a761e38926d8c5a5c1cdee5a8f85e
Thompadude <thompa.lansing@gmail.com>
2015-10-27 18:53:59 +0100

    Refaktorerat CompanyManage

--- src/managers/CompanyManage.java
+++ src/managers/CompanyManage.java
@@ -18,21 +18,17 @@ public class CompanyManage {
             desiredCompanyName = getCompanyNameFromUserInput(businessAssociate, stringScanner);
 
             if (!objManage.getErrorManage().catchArrayListNullPointerException(myCompany.getBusinessAssociates())) {
-
                 for (int i = 0; i < myCompany.getBusinessAssociates().size(); i++) {
 
                     currentCompanyName = myCompany.getBusinessAssociates().get(i).getCompany().getName();
 
                     if (currentCompanyName.toLowerCase().equals(desiredCompanyName.toLowerCase())) {
-                        System.out.println("Company " + currentCompanyName + " already exists!");
-                        System.out.print("Do you want to add " + businessAssociate + " to " + currentCompanyName + ", or change company? ");
-                        System.out.print("[1]Add to existing/[2]Change company: ");
+                        companyAlreadyExistWarning(currentCompanyName, businessAssociate);
+
                         userInputMenuChoice = objManage.getErrorManage().catchUserInputMismatchException(intScanner);
 
                         if (userInputMenuChoice == 1) {
-                            System.out.println(businessAssociate + " added to " + currentCompanyName + ".");
-                            Company company = myCompany.getBusinessAssociates().get(i).getCompany();
-                            return company;
+                            return addCurrentBusinessAssociateToExistingCompany(myCompany, businessAssociate, currentCompanyName, i);
                         } else if (userInputMenuChoice == 2) {
                             i = myCompany.getBusinessAssociates().size();
                             goAgain = true;
@@ -62,6 +58,21 @@ public class CompanyManage {
     private String getCompanyNameFromUserInput(String businessAssociate, Scanner stringScanner) {
         System.out.print("\nEnter some information about the company where " + businessAssociate + " works.\nName: ");
         return stringScanner.nextLine();
+    }
+
+    private Company addCurrentBusinessAssociateToExistingCompany(
+            MyCompany myCompany,
+            String businessAssociate,
+            String currentCompanyName,
+            int arrayIndexOfCompany) {
+        System.out.println(businessAssociate + " added to " + currentCompanyName + ".");
+        return myCompany.getBusinessAssociates().get(arrayIndexOfCompany).getCompany();
+    }
+
+    private void companyAlreadyExistWarning(String currentCompanyName, String businessAssociate) {
+        System.out.println("Company " + currentCompanyName + " already exists!");
+        System.out.print("Do you want to add " + businessAssociate + " to " + currentCompanyName + ", or change company? ");
+        System.out.print("[1]Add to existing/[2]Change company: ");
     }
 
 }
